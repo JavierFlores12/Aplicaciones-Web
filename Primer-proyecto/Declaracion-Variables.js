b9d8b08599022948f1e54557d5ee7dd950319717
JavierFlores12 <135484983+JavierFlores12@users.noreply.github.com>
2024-06-19 21:50:03 -0600

    Add files via upload

---
+++ Primer-proyecto/Declaracion-Variables.js
@@ -0,0 +1,22 @@
+//¿Que es una varaible? Es un caracter que tiene la capacidad de contener informacion y valores.
+//Declaracion de variables
+var variable = 2; //Sirve para variables de instancia
+let variablelet = 2; //Sirve para declarar variables locales
+
+var x=2;
+console.log(x);
+x = 7;
+console.log(x);
+
+let z=3;
+console.log(z);
+ 
+z = 5;
+console.log(z);
+
+var variable = _"Soy una varible tipo VAR"
+
+for(var i=0; i<2; i++){
+    let ejemlet = "Ejemplo de varible let"
+}
+console.log(variable)
